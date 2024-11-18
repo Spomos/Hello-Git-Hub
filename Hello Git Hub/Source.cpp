82561460257fc338fa85eb7d03a9366807e42621
pcuser <pcuser@DESKTOP-B2UII6D>
2024-11-18 16:38:14 -0500

    Add project files.

---
+++ Hello Git Hub/Source.cpp
@@ -0,0 +1,13 @@
+//Sarah Mostrom
+//1202 Prof. Bowman
+//Nov. 18, 2024
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	cout << "Hello, world!" << endl << endl;
+
+	return 0;
+}
